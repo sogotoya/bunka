ecc24d53b144e7034b33771f08c376d7a6610a62
sogotoya <24051904@d.anabuki-college.net>
2024-12-18 15:05:57 +0900

    count、テキスト追加

--- bunka/Project/GameProject/Game/count.cpp
+++ bunka/Project/GameProject/Game/count.cpp
@@ -5,7 +5,10 @@
 count::count(const CVector2D& pos) : Base(eType_count),
 m_count_text("C:\\Windows\\Fonts\\msgothic.ttc", 100)
 {
-	m_cnt = 180 * 60;
+	m_cnt = 100 * 60;
+	//stage10‚Ìê‡‚Ì‚İ40•b
+	if (GameData::s_score == 10)
+		m_cnt = 40 * 60;
 }
 
 
